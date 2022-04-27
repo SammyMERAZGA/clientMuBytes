b51df7b57288e51cce88f480681b456091cccab8
SammyMERAZGA <sammy.merazga@gmail.com>
2022-04-27 09:35:33 +0200

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
+    <Sidebar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import Sidebar from "./components/Sidebar/Sidebar.vue";
+
+@Component({
+  components: {
+    Sidebar,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  font-family: "Poppins", sans-serif;
 }
 </style>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Sidebar</h1>
+  </div>
+</template>
+
+<script lang="ts" src="./index.ts"></script>
+<style src="./style.css"></style>
